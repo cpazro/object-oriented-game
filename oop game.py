f1305869d2d7d1885c1d40fce4e97bb39ac4d6bb
Catalina Rodríguez <cpaz.rodriguez@uc.cl>
2023-01-01 11:04:11 -0300

    Update oop game.py

--- oop game.py
+++ oop game.py
@@ -12,17 +12,30 @@ class GameObject:
         self.feel = feel
         self.smell = smell
 
+    # Return string describing object appearance
     def look(self):
         return f"You look at the {self.name}. {self.appearance}\n"
-
+    # Return string describing object feel 
     def touch(self):
         return f"You touch the {self.name}. {self.feel}\n"
-
+    # Return string describing smell
     def sniff(self):
         return f"You sniff the {self.name}. {self.smell}\n"
     
-game_object = GameObject('Knife', 'Some appearance', 'Some feel', 'Some smell')
+class Room:
 
-game_object.name = "Spoon"
-print(game_object.name)
-print(game_object.sniff())
+    escape_code = 0
+    game_objects = []
+
+    def __init__(self, escape_code, game_objects):
+        self.escape_code = escape_code
+        self.game_objects = game_objects
+
+    def check_code(self, code):
+        return self.escape_code == code
+    
+    def get_game_object_names(self):
+        names = []
+        for object in self.game_objects:
+            names.append(object.name)
+        return names
